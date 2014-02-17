6911eebfc68cfa92ec05e79bb8aacd47c8f863bb
jcomello <joao@deoliveiraemello.com>
2014-02-17 00:10:29 -0300

    backbone setup

---
+++ app/assets/javascripts/white_board.js
@@ -0,0 +1,13 @@
+window.WhiteBoard = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function () {
+    alert('Hello from Backbone!');
+  },
+};
+
+$(document).ready(function() {
+  WhiteBoard.initialize();
+});
